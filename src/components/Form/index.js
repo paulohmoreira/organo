00adc8d62d9cd922a30967fa89b9c465588f4a9f
Paulo Moreira <phmoreira37@gmail.com>
2022-12-19 16:18:52 -0300

    Iniciado cadastro de colaboradores

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -4,7 +4,7 @@ import InputText from '../InputText';
 import ListDropdown from '../ListDropdown';
 import './Form.css';
 
-const Form = () => {
+const Form = (props) => {
   const times = [
     'Programação',
     'Front-End',
@@ -22,7 +22,12 @@ const Form = () => {
 
   const aoSalvar = (event) => {
     event.preventDefault();
-    console.log('Form foi submetido => ', nome, cargo, imagem, time);
+    props.aoColaboradorCadastrado({
+      nome,
+      cargo,
+      imagem,
+      time,
+    });
   };
 
   return (
