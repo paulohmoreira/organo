82083462f917f8e40c7472d7203611ebad0d339c
Paulo Moreira <phmoreira37@gmail.com>
2022-12-20 09:19:55 -0300

    Passando props para o Time

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ import Form from './components/Form';
 import Time from './components/Time';
 
 function App() {
+  const times = [
+    { nome: 'Programação', corPrimaria: '#57C278', corSecundaria: '#D9F7E9' },
+    { nome: 'Front-End', corPrimaria: '#82CFFA', corSecundaria: '#E8F8FF' },
+    { nome: 'Data Science', corPrimaria: '#A6D157', corSecundaria: '#F0F8E2' },
+    { nome: 'Devops', corPrimaria: '#E06B69', corSecundaria: '#FDE7E8' },
+    { nome: 'UX e Design', corPrimaria: '#DB6EBF', corSecundaria: '#FAE9F5' },
+    { nome: 'Mobile', corPrimaria: '#FFBA05', corSecundaria: '#FFF5D9' },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF',
+    },
+  ];
+
   const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
@@ -19,10 +33,14 @@ function App() {
           aoNovoColaboradorAdicionado(colaborador)
         }
       />
-      <Time nome='Programação' />
-      <Time nome='Front-End' />
-      <Time nome='Data Science' />
-      <Time nome='Devops' />
+      {times.map((time) => (
+        <Time
+          nome={time.nome}
+          key={time.nome}
+          corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria}
+        />
+      ))}
     </div>
   );
 }
