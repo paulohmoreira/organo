db51c82fa7d169b75c1430cc2ffb18260953c6e4
Paulo Moreira <phmoreira37@gmail.com>
2022-12-20 18:41:51 -0300

    Criado component Colcaborador

---
+++ src/components/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css';
+
+const Colaborador = () => {
+  return (
+    <div className='colaborador'>
+      <div className='cabecalho'>
+        <img src='https://github.com/paulohmoreira.png' alt='Paulo Moreira' />
+      </div>
+      <div className='rodape'>
+        <h4>Paulo Moreira</h4>
+        <h5>Progamador</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
